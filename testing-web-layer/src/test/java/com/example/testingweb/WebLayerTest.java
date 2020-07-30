c9af50ecfdeda06c7c81fa3ca3841678fda6e81d
Liz Schilb <eschilb@gmail.com>
2020-07-30 15:09:19 -0400

    update weblayertest

--- testing-web-layer/src/test/java/com/example/testingweb/WebLayerTest.java
+++ testing-web-layer/src/test/java/com/example/testingweb/WebLayerTest.java
@@ -1,17 +1,19 @@
 package com.example.testingweb;
 
-import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest
+@WebMvcTest(HomeController.class)
+//tag::test[]
 public class WebLayerTest {
 
 	@Autowired
@@ -23,3 +25,4 @@ public class WebLayerTest {
 				.andExpect(content().string(containsString("Hello, World")));
 	}
 }
+//end::test[]
